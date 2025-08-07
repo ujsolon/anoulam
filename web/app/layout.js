b5aa997a9437b29ab46e2757c189459ea092ed55
ujsolon <ujsolon@gmail.com>
2025-08-07 14:28:35 +0800

    Add "@vercel/analytics": "^1.5.0" to dependencies and Analytics in layout.js

--- web/app/layout.js
+++ web/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: 'Ano Ulam',
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="layout-body">
         {children}
+        <Analytics />
       </body>
     </html>
   );
